53bd6e90ea5d4d55c34ea08ae6eb46a8e1124c13
MARIA EDUARDA ZABEL <maria_e_zabel@estudante.sesisenai.org.br>
2025-11-25 21:52:09 -0300

    MZ feat: implementa inicio

---
+++ src/main/java/org/example/repository/EquipamentoRepository.java
@@ -0,0 +1,69 @@
+package org.example.repository;
+
+import org.example.database.Conexao;
+import org.example.model.Equipamento;
+
+import java.sql.*;
+
+public class EquipamentoRepository {
+
+    public Equipamento criarEquipamento(Equipamento equipamento)throws SQLException{
+        String query = "INSERT INTO Equipamento (nome, numeroDeSerie, areaSetor, statusOperacional) VALUES (?,?,?,?)";
+
+        try(Connection conn = Conexao.conectar();
+            PreparedStatement stmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)){
+
+            stmt.setString(1, equipamento.getNome());
+            stmt.setString(2, equipamento.getNumeroDeSerie());
+            stmt.setString(3, equipamento.getAreaSetor());
+            stmt.setString(4, equipamento.getStatusOperacional());
+            stmt.executeUpdate();
+
+            ResultSet rs = stmt.getGeneratedKeys();
+            if(rs.next()){
+                equipamento.setId(rs.getLong(1));
+            }
+        }
+
+        return equipamento;
+    }
+
+    public boolean equipamentoExiste(long id) throws  SQLException{
+        String query = "SELECT nome FROM Equipamento WHERE id = ?";
+
+        try(Connection conn = Conexao.conectar();
+            PreparedStatement stmt = conn.prepareStatement(query)){
+
+            stmt.setLong(1, id);
+
+            ResultSet rs = stmt.executeQuery();
+
+            if(rs.next()){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public Equipamento BuscarEquipamentoPorID(long id) throws SQLException{
+        String query = "SELECT id, nome, numeroDeSerie, areaSetor, statusOperacional FROM Equipamento WHERE id = ?";
+
+        try(Connection conn = Conexao.conectar();
+            PreparedStatement stmt = conn.prepareStatement(query)){
+
+            stmt.setLong(1, id);
+
+            ResultSet rs = stmt.executeQuery();
+
+            long ID = rs.getLong("id");
+            String nome = rs.getString("nome");
+            String numeroDeSerie = rs.getString("numeroDeSerie");
+            String areaSetor = rs.getString("areaSetor");
+            String statusOperacional = rs.getString("statusOperacional");
+
+            var equipamento = new Equipamento(ID, nome, numeroDeSerie, areaSetor, statusOperacional);
+            return equipamento;
+        }
+    }
+}
--- src/main/java/org/example/service/equipamento/EquipamentoServiceImpl.java
+++ src/main/java/org/example/service/equipamento/EquipamentoServiceImpl.java
@@ -1,17 +1,29 @@
 package org.example.service.equipamento;
 
 import org.example.model.Equipamento;
+import org.example.repository.EquipamentoRepository;
 
 import java.sql.SQLException;
 
 public class EquipamentoServiceImpl implements EquipamentoService{
+
+    EquipamentoRepository repositoryE = new EquipamentoRepository();
+
     @Override
     public Equipamento criarEquipamento(Equipamento equipamento) throws SQLException {
-        return null;
+        equipamento.setStatusOperacional("OPERACIONAL");
+        return repositoryE.criarEquipamento(equipamento);
     }
 
     @Override
     public Equipamento buscarEquipamentoPorId(Long id) throws SQLException {
-        return null;
+        boolean existe = repositoryE.equipamentoExiste(id);
+
+        if(existe == true){
+            Equipamento equipamento = repositoryE.BuscarEquipamentoPorID(id);
+            return equipamento;
+        }
+
+        throw new RuntimeException("Equipamento não encontrado!");
     }
 }
